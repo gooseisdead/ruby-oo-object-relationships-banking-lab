561157597dc6ce1bafda1413e582973c4a15bc2c
gooseisdead <gooseisdead@mac.com>
2020-09-18 13:22:44 -0400

    Done /bMoon_PP exit clear

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
-class BankAccount
+class BankAccount 
+    attr_accessor :balance, :status
+    attr_reader :name
+    attr_writer
 
+    @@all = []
+      
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self  
+    end
+
+    def self.all
+        @@all
+    end
+
+    def deposit(amount)
+        @balance +=amount
+    end
+
+    def display_balance
+        p "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if self.status == "open" && self.balance > 0
+            return true
+        else 
+            return false
+        end
+    end
+
+    def close_account
+        @status = "closed"
+        @balance = 0
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,48 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  attr_reader 
+  attr_writer
+
+  @@all = []
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid? == true
+      return true
+    else
+      return false
+    end
+  end
+
+  def execute_transaction
+    if @sender.balance >= @amount && @status == "pending" && self.valid? == true
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete" 
+    else
+      @status = "rejected"
+      p "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete" 
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      self.status = "reversed"
+    end
+  end
+ 
 end
